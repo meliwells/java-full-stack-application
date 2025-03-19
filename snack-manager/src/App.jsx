6f587352b0caefc3b478a4290891ca7d777a6e38
Melissa Wells <meliwells08@gmail.com>
2025-03-19 10:37:01 -0600

    working on fetching data

--- snack-manager/src/App.jsx
+++ snack-manager/src/App.jsx
@@ -1,12 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 export default function App() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+
+    useEffect(() => {
+      fetch('http://localhost:8080/disneySnacks/snacks')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Something went wrong. Please try again later</p>;
+  
+    return (
+      <div>
+        <h1>Snack List</h1>
+        <ul>
+          {data.map(snacks => (
+            <li key={snacks.snacksId} className='snack-item'>
+              <div>
+              <strong>{snacks.title}</strong> - ${snacks.price} 
+              </div>
+              <p>{snacks.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 
 
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+  // return (
+  //   <div className="App">
+  //     <h1>Hello World!</h1>
+  //   </div>
+  // );
+
+
--- snack-manager/src/utilities/api.Utilities.js
+++ snack-manager/src/utilities/api.Utilities.js
@@ -1,11 +1,1 @@
 
-const getSnacks = () => {
-    return fetch('http://localhost:8080/snacks', 
-        {
-          method: 'GET',
-          headers: {'Content-Type': 'application/json'},
-        })
-        .then(response => response.json())
-}
-
-export {getSnacks}
